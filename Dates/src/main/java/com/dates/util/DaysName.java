5e18bcab4b25d419377ce2be87c5bf3b7f6a908f
Marta Jurado Ruiz <mjurado@grupocmc.es>
2015-01-26 14:22:41 +0100

    Nueva Branch Dates

---
+++ Dates/src/main/java/com/dates/util/DaysName.java
@@ -0,0 +1,14 @@
+package com.dates.util;
+
+import java.text.DateFormatSymbols;
+
+public class DaysName {
+	 public static void main(String[] args) {
+	      String[] weekdays = new DateFormatSymbols().getWeekdays();
+	      for (int i = 2; i < (weekdays.length-1); i++) {
+	         String weekday = weekdays[i];
+	         System.out.println(weekday);
+	      }
+	     
+	   }
+}
